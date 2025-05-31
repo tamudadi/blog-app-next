a17930483f090c35953464c54653befef3181b96
tamudadi <177226849+UserTamura@users.noreply.github.com>
2025-05-31 18:12:03 +0900

    feat: 記事一覧ページと記事詳細ページを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,60 @@
+//INFO:useEffect や useState などの クライアントサイドの機能 を使う場合は、ファイルの先頭に'use client'の記述が必要。
+'use client';
+
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { Post } from '../../types/post';
+
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const postsFetch = async () => {
+      const res = await fetch(
+        'https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts'
+      );
+      const { posts } = await res.json();
+      setPosts(posts);
+    };
+
+    postsFetch();
+  }, []);
+
   return (
     <>
-      <div>
-        記事一覧
-        <ul></ul>
+      <div className="my-10 mx-6">
+        <ul className="flex flex-col gap-10">
+          {posts.map((post) => {
+            return (
+              <li key={post.id} className="border p-4">
+                <Link href={`/posts/${post.id}`}>
+                  <div className="flex justify-between">
+                    <div className="text-sm text-gray-500">
+                      {new Date(post.createdAt).toLocaleDateString()}
+                    </div>
+                    <div className="flex gap-2">
+                      {post.categories.map((category) => {
+                        return (
+                          <div
+                            key={category}
+                            className="border border-blue-500 rounded text-blue-500 text-sm px-2 py-1"
+                          >
+                            {category}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <h1 className="text-2xl mt-2 mb-4">{post.title}</h1>
+                  <p
+                    className="line-clamp-2"
+                    dangerouslySetInnerHTML={{ __html: post.content }}
+                  />
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,7 +1,66 @@
+'use client';
 /*  INFO: page.tsx はファイル名ベースでルーティングされる（動的ルーティング）仕組みの一部
  ディレクトリ構造がそのままURLパスになる
 [id] のように [] を使うと動的ルーティングになり、URLの一部を変数として受け取れる */
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Post } from '../../../../types/post';
 
-export default function page() {
-  return <div>page</div>;
+export default function Page() {
+  const [post, setPost] = useState<Post | null>(null);
+  const { id } = useParams<{ id: string }>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const postFetch = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`
+        );
+        const { post } = await res.json();
+        setPost(post);
+      } catch (error) {
+        console.error(error);
+        setError('記事の取得に失敗しました。');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    postFetch();
+  }, [id]);
+
+  if (error) return <div>{error}</div>;
+  if (isLoading) return <div>読み込み中...</div>;
+  if (!post) return <div>記事が見つかりません</div>;
+
+  return (
+    <>
+      <div className="my-14 mx-6">
+        <Image src={post.thumbnailUrl} alt="" height={1000} width={1000} />
+        <div className="flex justify-between pt-4">
+          <div className="text-sm text-gray-500">
+            {new Date(post.createdAt).toLocaleDateString()}
+          </div>
+          <div className="flex gap-2">
+            {post.categories.map((category) => {
+              return (
+                <div
+                  key={category}
+                  className="border border-blue-500 rounded text-blue-500 text-sm px-2 py-1"
+                >
+                  {category}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <h1 className="text-2xl mt-2 mb-4">{post.title}</h1>
+        <p dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    </>
+  );
 }
