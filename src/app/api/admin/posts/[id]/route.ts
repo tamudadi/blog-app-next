2d17c7e2a23c299eed3b51cc4451fd67c2e83274
tamudadi <177226849+UserTamura@users.noreply.github.com>
2025-06-06 00:14:58 +0900

    feat: 記事詳細取得APIを実装

---
+++ src/app/api/admin/posts/[id]/route.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+// GETという命名にすることで、GETリクエストの時にこの関数が呼ばれる
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const id = params.id;
+  try {
+    // Postの一覧をDBから取得
+    const post = await prisma.post.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      include: {
+        postCategories: {
+          include: {
+            category: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    // レスポンスを返す
+    return NextResponse.json({ status: 'OK', post: post }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
+  }
+};
