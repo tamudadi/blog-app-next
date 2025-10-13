83bb198f9b234cd88f5869c648d7ca6805197956
tamudadi <177226849+UserTamura@users.noreply.github.com>
2025-10-13 20:42:25 +0900

    feat: カテゴリーの更新と削除機能を追加

--- src/app/api/admin/categories/[id]/route.ts
+++ src/app/api/admin/categories/[id]/route.ts
@@ -29,3 +29,41 @@ export const GET = async (
       return NextResponse.json({ status: error.message }, { status: 400 });
   }
 };
+
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+  const { name } = await request.json();
+
+  try {
+    const category = await prisma.category.update({
+      where: { id: parseInt(id) },
+      data: { name },
+    });
+
+    return NextResponse.json({ status: 'OK', category }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
+  }
+};
+
+export const DELETE = async (
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+
+  try {
+    await prisma.category.delete({
+      where: { id: parseInt(id) },
+    });
+
+    return NextResponse.json({ status: 'OK' }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 });
+  }
+};
