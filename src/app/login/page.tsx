7fda0516a1b17acb03c3f839b39072838e4f155f
tamudadi <177226849+UserTamura@users.noreply.github.com>
2025-10-22 00:04:32 +0900

    feat:ログインページをRHF対応に変更

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,85 +2,86 @@
 
 import { supabase } from '@/utils/supabase';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export default function Page() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsSubmitting(true);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>();
 
-    try {
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-
-      if (error) {
-        alert('メールアドレスまたはパスワードが正しくありません。');
-        return;
-      }
-
-      //errorプロパティがなければ/admin/postsへ
-      router.replace('/admin/posts');
-    } finally {
-      setIsSubmitting(false);
+  const onSubmit = async (data: FormData) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email: data.email,
+      password: data.password,
+    });
+    if (error) {
+      alert('メールアドレスまたはパスワードが正しくありません。');
+      return;
     }
+    ('ログインしました');
+    //errorプロパティがなければ/admin/postsへ
+    router.replace('/admin/posts');
   };
 
   return (
     <div className="flex justify-center pt-[240px]">
-      <form onSubmit={handleSubmit} className="space-y-4 w-full max-w-[400px]">
-        <div>
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900"
-          >
-            メールアドレス
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="name@company.com"
-            required
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900"
-          >
-            パスワード
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="••••••••"
-            required
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="space-y-4 w-full max-w-[400px]"
+      >
+        <label
+          htmlFor="email"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          メールアドレス
+        </label>
+        <input
+          type="email"
+          placeholder="name@company.com"
+          {...register('email', {
+            required: true,
+            pattern: /^\S+@\S+$/i,
+          })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        />
+        {errors.email && (
+          <p className="text-red-500 text-sm">
+            メールアドレスを正しく入力してください
+          </p>
+        )}
 
-        <div>
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-          >
-            {isSubmitting ? 'ログイン中....' : 'ログイン'}
-          </button>
-        </div>
+        <label
+          htmlFor="password"
+          className="block mb-2 text-sm font-medium text-gray-900"
+        >
+          パスワード
+        </label>
+        <input
+          type="password"
+          placeholder="••••••••"
+          {...register('password', { required: true, minLength: 6 })}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        />
+        {errors.password && (
+          <p className="text-red-500 text-sm">
+            パスワードは6文字以上で入力してください
+          </p>
+        )}
+        <input
+          type="submit"
+          disabled={isSubmitting}
+          value={isSubmitting ? 'ログイン中...' : 'ログイン'}
+          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        />
       </form>
     </div>
   );
